7ed256d9345f105042647d32d38080a2f31fddf4
edubcn75 <eduardobcn75@hotmail.com>
2025-10-16 21:40:02 +0200

    GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  // base: "/shoppingreact", //localhost
+  base: "/conference_event_planner", //github pages
   plugins: [react()],
 })
